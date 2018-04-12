126f6db0d91143f845ee528009c21bcef82693c7
Eugene Gornysh <e.gornysh@gmail.com>
2018-04-12 21:47:30 +0300

    test optimistaion

--- add_group.py
+++ add_group.py
@@ -22,7 +22,6 @@ class add_group(unittest.TestCase):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").send_keys("\\undefined")
         wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_id("LoginForm").click()
