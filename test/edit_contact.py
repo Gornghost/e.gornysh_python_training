c469a278fded929e0676760d21392e56b64b9fe6
Gornghost <e.gornysh@gmail.com>
2018-04-20 19:05:12 +0300

    adding new test 'edit_contact'

---
+++ test/edit_contact.py
@@ -0,0 +1,15 @@
+from model.contact import Contact
+
+
+def test_edit_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first(Contact(first_name=" EDITED", middle_name=" EDITED", last_name=" EDITED", nickname=" EDITED",
+                        title=" EDITED", company=" EDITED", address=" EDITED", home_phone=" EDITED",
+                        mobile_phone=" EDITED", work_phone=" EDITED", fax=" EDITED", email1=" EDITED",
+                        email2=" EDITED", email3=" EDITED", homepage_link=" EDITED",
+                        birthday_day="//div[@id='content']/form/select[1]//option[3]",
+                        birthday_month="//div[@id='content']/form/select[2]//option[3]", birthday_year=" EDITED",
+                        anniversary_day="//div[@id='content']/form/select[3]//option[3]",
+                        anniversary_month="//div[@id='content']/form/select[4]//option[3]", anniversary_year=" EDITED",
+                        secondary_address=" EDITED", home_phone2=" EDITED", notes=" EDITED"))
+    app.session.logout()
--- test/edit_group.py
+++ test/edit_group.py
@@ -1,7 +1,7 @@
 from model.group import Group
 
 
-def test_edit_group(app):
+def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.edit_first(Group(name=" EDITED", header=" EDITED", footer=" EDITED"))
     app.session.logout()
