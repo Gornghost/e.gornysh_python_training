3e2f20fe98b1c0aeec1f2ceb269f2fd58e5f5494
Gornghost <e.gornysh@gmail.com>
2018-05-03 12:26:56 +0300

    added checking of equal lists for test add/modify/delete group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,18 +2,29 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="TEST"))
     app.group.edit_first(Group(name=" NAME EDITED"))
+    new_groups = app.group.get_group_list()
+    assert len(new_groups) == len(old_groups)
 
 
 def test_edit_first_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="TEST"))
     app.group.edit_first(Group(header=" HEADER EDITED"))
+    app.group.edit_first(Group(name=" NAME EDITED"))
+    new_groups = app.group.get_group_list()
+    assert len(new_groups) == len(old_groups)
 
 
 def test_edit_first_group_footer(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="TEST"))
     app.group.edit_first(Group(footer=" FOOTER EDITED"))
+    app.group.edit_first(Group(name=" NAME EDITED"))
+    new_groups = app.group.get_group_list()
+    assert len(new_groups) == len(old_groups)
